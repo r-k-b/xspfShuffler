80e3f4beeb93e842567fbda1ba1592ac88fb55e3
rtzarius <rtzarius@gmail.com>
2013-02-17 13:19:09 +1100

    Initialise with empties.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Robert K. Bell',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'rtzarius@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'}
+
+setup(**config)
+
