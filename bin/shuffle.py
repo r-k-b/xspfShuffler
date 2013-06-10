6b9eb5a9122b25f57a36317341169093c611bba3
rtzarius <rtzarius@gmail.com>
2013-06-10 15:21:53 +1000

    Scrap obsolete code.
    Reading from existing playlist doesn't suit the workflow I envision:
    Drop a new file into a folder, run the Shuffler, get a new playlist
    file.

--- bin/shuffle.py
+++ bin/shuffle.py
@@ -14,62 +14,40 @@ ET.register_namespace('', 'http://xspf.org/ns/0/')
 
 ET.register_namespace('vlc', 'http://www.videolan.org/vlc/playlist/ns/0/')
 
-def loadPlaylists():
-    if not os.path.isdir(playlistFolder):
-        print "Do you have a subdirectory named '{0}' ?".format(playlistFolder)
-        raise IOError('Didn\'t find the expected subfolder named \'{0}\'.'.format(playlistFolder))
-    
-    try:
-        listA = ET.parse(os.path.join(playlistFolder, 'ListA.xspf'))
-        listB = ET.parse(os.path.join(playlistFolder, 'ListB.xspf'))
-        listC = ET.parse(os.path.join(playlistFolder, 'ListC.xspf'))
-    except (IOError, ET.ParseError) as err:
-        print "Error: {0}: {1}".format(type(err), err)
-        print ("Do you have valid VLC playlists named ListA.xspf, " 
-            "ListB.xspf, and ListC.xspf in the subfolder?")
-    
-    return listA, listB, listC
+def getSourceDirs():
+    """
+    This will accept commandline arguments or config file settings, and return
+    a dict of directories from which to source the media files.
+    For now it is hard-coded.
+    """
+    media = {}
+    hardCodedSource = 'C:\\Python27\\projects\\xspfShuffler\\media\\'
+    media['a'] = os.path.join(hardCodedSource, 'a') # Programmes (long)
+    media['b'] = os.path.join(hardCodedSource, 'b') # Ad breaks (medium)
+    media['c'] = os.path.join(hardCodedSource, 'c') # interstitials (short)
+    return media
 
-def slideChildren(element, tag = "track"):
-    "Takes an ElementTree element, renumbers the children of same by a random offset."
-
-    numChildren = len(element)
-    offset = randint(0, numChildren)
-        
-    newElement = element
-    for child in newElement.findall(tag): # 'track'
-        root.remove(child)
-    
-    for newtrack in range(playlist_length):
-        oldtrack = (newtrack + playlist_offset) % playlist_length
-        print oldtrack, newtrack
-        playlist_jumbledroot[1][oldtrack] = playlist_root[1][newtrack]
-    
-    
-    return newElement
-
-def joinPlaylists(listA, listB, listC):
+def joinLists(listA, listB, listC):
     pass
     
 def writeNewPlaylist(playlist):
     pass
    
 def main():
-    listA, listB, listC = loadPlaylists()
-    listD = joinPlaylists(listA, listB, listC)
-    writeNewPlaylist(listD)
+    mediaPaths = getSourceDirs()
+    
+    # fill lists from contents of paths
+
+    # jumble lists
+
+    # merge lists in described order
+    joinLists()
+
+    # write out new playlist to current folder
+    writeNewPlaylist()
+    
 
 if __name__ == "__main__":
     os.chdir('C:\\Python27\\projects\\xspfShuffler')
     main()
 
-# testing - disable these
-print __name__
-a,b,c = loadPlaylists()
-ar = a.getroot()
-br = b.getroot()
-cr = c.getroot()
-a.findall('{http://xspf.org/ns/0/}extension/*')
-a.findall('{http://xspf.org/ns/0/}tracklist/*')
-d = jumblePlaylist(a)
-d.write('playlists\\d-test.xspf')
